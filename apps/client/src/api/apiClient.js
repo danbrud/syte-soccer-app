2de6b692bf9f92b56501bf1e45bf21f736b3fe5a
danbrud <dannybrudner@users.noreply.github.com>
2021-01-20 12:32:22 +0200

    removed local host from client

--- apps/client/src/api/apiClient.js
+++ apps/client/src/api/apiClient.js
@@ -2,12 +2,12 @@ import axios from 'axios'
 
 export const apiClient = {
   fetchTeams: (page) => {
-    return axios.get(`http://localhost:3000/api/teams?page=${page}`)
+    return axios.get(`/api/teams?page=${page}`)
   },
   saveTeam: (teamId) => {
-    return axios.post('http://localhost:3000/api/teams', { teamId })
+    return axios.post('/api/teams', { teamId })
   },
   deleteTeam: (teamId) => {
-    return axios.delete(`http://localhost:3000/api/teams/${teamId}`)
+    return axios.delete(`/api/teams/${teamId}`)
   }
 }
